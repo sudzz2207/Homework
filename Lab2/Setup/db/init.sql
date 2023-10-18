7d8f9c4610f4655a351e6f41bdef8cfd9d3a8004
sudzz2207 <102476288+sudzz2207@users.noreply.github.com>
2023-10-18 09:49:01 -0600

    init.sql

--- Lab2/Setup/db/init.sql
+++ Lab2/Setup/db/init.sql
@@ -4,7 +4,6 @@ CREATE TABLE outcome (
     ts TIMESTAMP,
     dob DATE,
     outcome_type VARCHAR,
-    outcome_type_id INT,
     outcome_subtype VARCHAR,
     animal_type VARCHAR,
     age VARCHAR,
@@ -25,28 +24,26 @@ CREATE TABLE animal (
 );
 
 CREATE TABLE outcome_type (
-    outcome_type_id INT PRIMARY KEY,
+    outcome_subtype INT PRIMARY KEY,
     outcome_type VARCHAR(20)
 );
 
-CREATE TABLE outcome_events (
+CREATE TABLE outcomes (
     outcome_event_id INT PRIMARY KEY,
     datetime TIMESTAMP,
     sex VARCHAR(20),
     outcome_subtype VARCHAR(20),
     animal_id VARCHAR(20),
-    outcome_type_id INT,
+    outcome_subtype VARCHAR,
     FOREIGN KEY (animal_id) REFERENCES animal(animal_id),
-    FOREIGN KEY (outcome_type_id) REFERENCES outcome_type(outcome_type_id)
+    FOREIGN KEY (outcome_subtype) REFERENCES outcome_type(outcome_subtype)
 );
 
 CREATE TABLE fact_table (
     animal_id VARCHAR(20) NOT NULL,
-    outcome_type_id INT,
+    outcome_subtype VARCHAR,
     outcome_event_id INT,
     FOREIGN KEY (animal_id) REFERENCES animal(animal_id),
-    FOREIGN KEY (outcome_type_id) REFERENCES outcome_type(outcome_type_id),
+    FOREIGN KEY (outcome_subtype) REFERENCES outcome_type(outcome_subtype),
     FOREIGN KEY (outcome_event_id) REFERENCES outcome_events(outcome_event_id)    
 );
-
-
