ff05001f57f47b5dfa7b1dca73bd852829dfea33
sudzz2207 <102476288+sudzz2207@users.noreply.github.com>
2023-10-18 09:51:28 -0600

    Update pipeline2.py

--- Lab2/Setup/pipeline2.py
+++ Lab2/Setup/pipeline2.py
@@ -13,10 +13,10 @@ def transform_data(data):
     data_new.columns = [col.lower() for col in data_new.columns]
 
     animal_columns = ['animal_id', 'breed', 'color', 'animal_name', 'animal_type', 'dob']
-    outcome_event_columns = ['outcome', 'datetime', 'sex', 'outcome_subtype', 'animal_id', 'outcome_subtype']
-    fact_table_columns = ['outcome', 'outcome_subtype', 'animal_id']
+    outcome_event_columns = ['outcomes', 'datetime', 'sex', 'outcome_subtype', 'animal_id', 'outcome_subtype']
+    fact_table_columns = ['outcomes', 'outcome_subtype', 'animal_id']
 
-    data_new['outcome'] = data_new.index + 1
+    data_new['outcomes'] = data_new.index + 1
 
     animal = data_new[animal_columns].drop_duplicates('animal_id', keep='first').reset_index(drop=True)
     unique_outcome_type = data_new[['outcome_type']].drop_duplicates().reset_index(drop=True)
@@ -26,27 +26,27 @@ def transform_data(data):
     outcome_subtype_map = dict(zip(unique_outcome_type['outcome_type'], unique_outcome_type['outcome_subtype']))
     data_new['outcome_subtype'] = data_new['outcome_type'].map(outcome_subtype_map)
 
-    outcome_events = data_new[outcome_event_columns]
+    outcomes = data_new[outcome_event_columns]
 
-    outcome_events.reset_index(drop=True, inplace=True)
+    outcomes.reset_index(drop=True, inplace=True)
 
     fact_table = data_new[fact_table_columns]
 
     print('Data Transformed')
-    return fact_table, animal, outcome_type, outcome_events
+    return fact_table, animal, outcome_type, outcomes
 
 def load_data(transformed_data):
     print('Loading data...')
 
-    fact_table, animal, outcome_type, outcome_events = transformed_data
+    fact_table, animal, outcome_type, outcomes = transformed_data
 
-    DATABASE_URL = "postgresql+psycopg2://sudha:asdf1234@0.0.0.0:5432/shelter"
+    DATABASE_URL = ("postgresql+psycopg2://sudha:asdf1234@0.0.0.0:5432/shelter")
 
     engine = create_engine(DATABASE_URL)
 
     animal.to_sql('animal', engine, if_exists='append', index=False)
     outcome_type.to_sql('outcome_type', engine, if_exists='append', index=False)
-    outcome_events.to_sql('outcome_events', engine, if_exists='append', index=False)
+    outcomes.to_sql('outcomes', engine, if_exists='append', index=False)
     fact_table.to_sql('fact_table', engine, if_exists='append', index=False)
 
     print('Data Loading Completed')
@@ -57,3 +57,4 @@ if __name__ == '__main__':
     transformed_data = transform_data(extracted_data)
     
     load_data(transformed_data)
+
